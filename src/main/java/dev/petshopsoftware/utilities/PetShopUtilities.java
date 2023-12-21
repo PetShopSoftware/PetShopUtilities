e48ad63acd2269d1e55f3bd55779b9459c480ef5
OmniscientARK <lorenzopiuga@gmail.com>
2023-12-21 03:17:47 -0300

    Remove unused main method

--- src/main/java/dev/petshopsoftware/utilities/PetShopUtilities.java
+++ src/main/java/dev/petshopsoftware/utilities/PetShopUtilities.java
@@ -1,11 +1,4 @@
 package dev.petshopsoftware.utilities;
 
-import dev.petshopsoftware.utilities.HTTP.Server.HTTPServer;
-
 public class PetShopUtilities {
-	public static void main(String[] args) {
-		new HTTPServer(8000)
-				.handlers(PetShopUtilities.class)
-				.start();
-	}
 }
