d694fa53959db94c4370cf8ec4da8d13b27b7f76
OmniscientARK <lorenzopiuga@gmail.com>
2023-12-21 02:49:01 -0300

    Fix exception handling

--- src/main/java/dev/petshopsoftware/utilities/Content/AbstractContent.java
+++ src/main/java/dev/petshopsoftware/utilities/Content/AbstractContent.java
@@ -13,50 +13,55 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @JsonDeserialize(using = AbstractContent.ContentDeserializer.class)
 public abstract class AbstractContent<T> implements JSON {
-    public static class ContentDeserializer extends JsonDeserializer<AbstractContent<?>> {
-        @Override
-        public AbstractContent<?> deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
-            return AbstractContent.read(jsonParser.getCodec().readTree(jsonParser));
-        }
-    }
+	public ContentType type;
+	public T value;
+	@JsonProperty("class")
+	public List<String> classList;
 
-    public ContentType type;
-    public T value;
-    @JsonProperty("class")
-    public List<String> classList;
+	protected AbstractContent(ContentType type, T value, String... classList) {
+		this.type = type;
+		this.value = value;
+		this.classList = Arrays.asList(classList);
+	}
 
-    protected AbstractContent(ContentType type, T value, String... classList) {
-        this.type = type;
-        this.value = value;
-        this.classList = Arrays.asList(classList);
-    }
+	public static AbstractContent<?> read(JsonNode contentJSON) throws Exception {
+		try {
+			ContentType type = ContentType.valueOf(contentJSON.get("type").asText());
+			String[] classList;
+			if (contentJSON.has("class"))
+				classList = MAPPER.convertValue(contentJSON.get("class"), new TypeReference<>() {
+				});
+			else classList = new String[0];
 
-    public static AbstractContent<?> read(JsonNode contentJSON) {
-        try {
-            ContentType type = ContentType.valueOf(contentJSON.get("type").asText());
-            String[] classList;
-            if (contentJSON.has("class"))
-                classList = MAPPER.convertValue(contentJSON.get("class"), new TypeReference<>() {
-                });
-            else classList = new String[0];
+			if (type == ContentType.BLOCK) {
+				List<JsonNode> contentJSONList = new ArrayList<>();
+				List<AbstractContent<?>> contentList = new ArrayList<>();
+				contentJSON.get("value").forEach(contentJSONList::add);
+				for (JsonNode contentValue : contentJSONList)
+					contentList.add(read(contentValue));
+				return new BlockContent(contentList, classList);
+			} else if (type == ContentType.TEXT) {
+				return new TextContent(contentJSON.get("value").asText(), classList);
+			} else if (type == ContentType.CARD) {
+				return new CardContent(read(contentJSON.get("value")), classList);
+			}
+		} catch (Exception e) {
+			throw new Exception("Failed to parse content:\n" + contentJSON.toPrettyString(), e);
+		}
+		throw new Exception("Parsed invalid content:\n" + contentJSON.toPrettyString());
+	}
 
-            if (type == ContentType.BLOCK) {
-                List<JsonNode> contentJSONList = new ArrayList<>();
-                contentJSON.get("value").forEach(contentJSONList::add);
-                List<AbstractContent<?>> contentList = contentJSONList.stream().map(AbstractContent::read).collect(Collectors.toList());
-                return new BlockContent(contentList, classList);
-            } else if (type == ContentType.TEXT) {
-                return new TextContent(contentJSON.get("value").asText(), classList);
-            } else if (type == ContentType.CARD) {
-                return new CardContent(read(contentJSON.get("value")), classList);
-            }
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to parse content:\n" + contentJSON.toPrettyString(), e);
-        }
-        throw new RuntimeException("Parsed invalid content:\n" + contentJSON.toPrettyString());
-    }
+	public static class ContentDeserializer extends JsonDeserializer<AbstractContent<?>> {
+		@Override
+		public AbstractContent<?> deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+			try {
+				return AbstractContent.read(jsonParser.getCodec().readTree(jsonParser));
+			} catch (Exception e) {
+				throw new IOException(e);
+			}
+		}
+	}
 }
