8228a2dcd6b3e705a084b9814ff31053b39f51fc
OmniscientARK <lorenzopiuga@gmail.com>
2023-12-22 14:38:40 -0300

    Fix changed database exceptions to be actual exceptions instead of RuntimeExceptions

--- src/main/java/dev/petshopsoftware/utilities/Database/DocumentReadException.java
+++ src/main/java/dev/petshopsoftware/utilities/Database/DocumentReadException.java
@@ -1,11 +1,11 @@
 package dev.petshopsoftware.utilities.Database;
 
-public class DocumentReadException extends RuntimeException {
-    public DocumentReadException(Exception exception) {
-        super(exception);
-    }
+public class DocumentReadException extends Exception {
+	public DocumentReadException(Exception exception) {
+		super(exception);
+	}
 
-    public DocumentReadException(String message) {
-        super(message);
-    }
+	public DocumentReadException(String message) {
+		super(message);
+	}
 }
--- src/main/java/dev/petshopsoftware/utilities/Database/DocumentWriteException.java
+++ src/main/java/dev/petshopsoftware/utilities/Database/DocumentWriteException.java
@@ -1,10 +1,11 @@
 package dev.petshopsoftware.utilities.Database;
 
-public class DocumentWriteException extends RuntimeException{
-    public DocumentWriteException(Exception exception) {
-        super(exception);
-    }
-    public DocumentWriteException(String message) {
-        super(message);
-    }
+public class DocumentWriteException extends Exception {
+	public DocumentWriteException(Exception exception) {
+		super(exception);
+	}
+
+	public DocumentWriteException(String message) {
+		super(message);
+	}
 }
