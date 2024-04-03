3dd4bf4ca171c015d9e459116d5945433df83d22
OmniscientARK <lorenzopiuga@gmail.com>
2024-04-03 04:56:43 -0300

    Updated default cached value

--- src/main/java/dev/petshopsoftware/utilities/Database/Mongo/IMongoDocument.java
+++ src/main/java/dev/petshopsoftware/utilities/Database/Mongo/IMongoDocument.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public interface IMongoDocument extends JSON {
-	boolean DEFAULT_CACHED = false;
+	boolean DEFAULT_CACHED = true;
 
 	default Document toDocument() {
 		return Document.parse(toJSON().toString());
--- src/main/java/dev/petshopsoftware/utilities/PetShopUtilities.java
+++
@@ -1,4 +0,0 @@
-package dev.petshopsoftware.utilities;
-
-public class PetShopUtilities {
-}
